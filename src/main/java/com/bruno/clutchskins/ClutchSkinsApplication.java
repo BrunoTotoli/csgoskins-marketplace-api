82615ea27a7a245048e5e4c26bd78d8149e62afa
BrunoTotoli <santosbruno101062@gmail.com>
2023-09-09 06:27:50 -0300

    project created, and dev profile to h2

---
+++ src/main/java/com/bruno/clutchskins/ClutchSkinsApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.clutchskins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClutchSkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClutchSkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bruno/clutchskins/ClutchSkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.clutchskins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClutchSkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
